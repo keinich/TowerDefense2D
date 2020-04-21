6175cfbeaab2579612243be9af2dca1374c6ac92
keinich <rafael.weschle@gmail.com>
2020-04-21 20:24:41 +0200

    Arrow stuff

--- Weapons/Projectile.gd
+++ Weapons/Projectile.gd
@@ -10,6 +10,10 @@ func _set_enemy(enemyToSet):
 func _process(delta):
 	if (enemy == null):
 		return
+	var distanveVector = enemy.global_position - position
+	var upVector = Vector2(0,1)
+	var angle = distanveVector.dot(upVector)
+	rotation = angle
 	position = position.linear_interpolate(enemy.global_position, delta * speed)
 
 func _on_ProjectileHitbox_area_entered(area):
